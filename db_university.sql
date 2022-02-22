df635bcfe3fe537be2ac71446a7b3ef763c4dd82
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-02-22 17:14:24 +0100

    done

--- db_university.sql
+++ db_university.sql
@@ -20,3 +20,4 @@ SELECT * FROM `degrees` WHERE `level` = "magistrale"
 SELECT COUNT(`id`) FROM `departments`
 
 -- 8. Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT * FROM `teachers` WHERE `phone` IS NULL
