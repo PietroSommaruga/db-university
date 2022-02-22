367713daf3e5cdc42f189f5456327e47426def9f
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-02-22 17:06:30 +0100

    5

--- db_university.sql
+++ db_university.sql
@@ -10,7 +10,8 @@ SELECT * FROM `students` WHERE (YEAR(CURRENT_DATE) - YEAR(`date_of_birth`)) >= 3
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 SELECT * FROM `courses` WHERE `year`= "1" AND `period` = "I semestre"
 
-
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT * FROM `exams` WHERE `date` = "2020-06-20" AND HOUR(`hour`) >= 14
+
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
 -- 7. Da quanti dipartimenti è composta l'università? (12)
