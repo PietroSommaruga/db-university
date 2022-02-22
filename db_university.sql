83b6386e53e318c7b6ea05f83103b86922c6c4ca
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-02-22 16:49:11 +0100

    3

--- db_university.sql
+++ db_university.sql
@@ -5,6 +5,8 @@ SELECT * FROM `students` where `date_of_birth` >= "1990-01-01" AND `date_of_birt
 SELECT * FROM `courses` WHERE `cfu` > "10"
 
 -- 3. Selezionare tutti gli studenti che hanno più di 30 anni
+SELECT * FROM `students` WHERE (YEAR(CURRENT_DATE) - YEAR(`date_of_birth`)) >= 30
+
 -- 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
 -- 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
 -- 6. Selezionare tutti i corsi di laurea magistrale (38)
