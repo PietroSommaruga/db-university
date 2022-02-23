6aca5e7eaa08d916adb1ce14276785def87d912b
PietroSommaruga <pietro.sommaruga2001@gmail.com>
2022-02-23 16:10:56 +0100

    2.2

--- db_university.sql
+++ db_university.sql
@@ -27,6 +27,10 @@ SELECT * FROM `teachers` WHERE `phone` IS NULL
 
 -- 1. Contare quanti iscritti ci sono stati ogni anno
 SELECT COUNT(*) AS "NUOVI STUDENTI 2021" FROM `students` WHERE `enrolment_date` LIKE "2021%" --(nuovi?)
+
 -- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(id) FROM `teachers` GROUP BY `office_address`
+
 -- 3. Calcolare la media dei voti di ogni appello d'esame
+
 -- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
